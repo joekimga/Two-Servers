c5e6a89f1ac9471f7fae00c1b68ce08250317d71
Joseph <joekimga@gmail.com>
2017-10-28 11:05:11 -0400

    WIP

--- Two-Servers.js
+++ Two-Servers.js
@@ -15,9 +15,10 @@ function handleRequest(request, response) {
 // Pass the handleRequest function to empower it with functionality.
 var server = http.createServer(handleRequest);
 
+var PORT = 7500;
 // Start our server so that it can begin listening to client requests.
 server.listen(PORT, function() {
 
   // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
+  console.log("I SUCK!" + PORT);
 });
